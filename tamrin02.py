1f72c5f1b96e1a399e1429bc47c4e194d313e1f8
Mahdi-Ghandali2003 <mahdi.ghandali2003@gmail.com>
2024-03-08 14:50:56 +0330

    check

--- tamrin02.py
+++ tamrin02.py
@@ -1,5 +1,5 @@
-a = []
-for i in range(1, 101): a.append(i)
+aa = []
+for i in range(1, 101): aa.append(i)
 
 b = []
 for j in range(2, 101, 2): b.append(j)
@@ -10,4 +10,4 @@ for h in range(1, 101): c.append(h ** 2)
 d = []
 for f in range(3, 101, 3): d.append(f ** 3)
 
-print(a, b, c, d)
+print(aa, b, c, d)
